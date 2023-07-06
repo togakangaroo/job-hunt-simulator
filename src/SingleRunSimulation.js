c2ed5eb1b8d554c2eee8318cd0fee6de0770cc34
togakangaroo <gmauer@gmail.com>
2023-07-05 19:23:31 -0500

    generate new sample button

--- src/SingleRunSimulation.js
+++ src/SingleRunSimulation.js
@@ -1,15 +1,22 @@
+import { useState, useEffect } from "react"
 import { sum, runSingleJobHuntSimulation, singleJobApplication } from "./analysis.js"
 import { period } from "./constants.js"
 
 export const SingleRunSimulation = ({ parameters }) => {
-  const periods = Array.from(runSingleJobHuntSimulation(parameters, () => singleJobApplication(parameters)))
-  const offerCount = sum(periods.map((p) => p.offers.size))
+  const [periods, setPeriods] = useState(null)
+  const runSingleSimulation = () => setPeriods(Array.from(runSingleJobHuntSimulation(parameters, () => singleJobApplication(parameters))))
+  useEffect(runSingleSimulation, [parameters])
+  const offerCount = sum(periods?.map((p) => p.offers.size) || [])
+  if (!periods) return null
   return (
     <article className="single-run-simulation">
       <header>Sample single job hunt simulation.</header>
       <p>
         This job hunt took {periods.length} {`${period}s`}. Ultimately receiving {offerCount} offers.
       </p>
+      <div className="actions">
+        <button onClick={runSingleSimulation}>Generate New Sample</button>
+      </div>
       <ul>
         {periods.map(({ unsuccessful, newApplicationCount, stageCounts }, period) => (
           <li key={period}>
